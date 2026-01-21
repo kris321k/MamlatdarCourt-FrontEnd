0ed38e71be7c1577ad3d95887627b0289f453e44
krutika-krutika <krutikaamonkar10@gmail.com>
2026-01-21 22:49:21 +0530

    Finalizing admin features and navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-import Contact from "./pages/contact";
+import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Help from "./pages/Help";
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <Link to="/About">About Us</Link>
       <Link to="/register">Register</Link>
       <Link to="/help">Help</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/Contact">Contact</Link>
       
     </nav>
   );
